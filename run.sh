2a8688fdd49a8c17df33458ea20c09ee253fe103
d.s.ulyanoff <d.s.ulyanoff@gmail.com>
2023-02-02 19:27:28 +0500

    add wait after db migration start

--- run.sh
+++ run.sh
@@ -2,16 +2,12 @@
 # run linters and tests
 go fmt ./... && go vet ./... && go test ./...
 # start mysql and profile service
-docker compose up -d
+docker-compose up -d
 # wait for mysql to warm up
 sleep 30
 # up migrations
 docker run -v ${PWD}/services/profile/migrations:/migrations --network host migrate/migrate -path=/migrations/ -database  'mysql://root:example@tcp(localhost:3306)/mysql' up 
+# wait for migrations to finish
+sleep 10
 # run tests
 docker run -it --rm --network host -v ${PWD}:/etc/newman -t postman/newman:alpine run otus-social-network.postman_collection.json --env-var "host=localhost"
-
-
-
-
-
-    
